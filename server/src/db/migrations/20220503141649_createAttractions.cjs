1f7820086320b0b08976fa8a15712c43fd6278e4
br-ndt <nlax76@gmail.com>
2022-05-04 18:15:06 -0400

    Merge branch 'main' into attraction-form-page

--- server/src/db/migrations/20220503141649_createAttractions.cjs
+++ server/src/db/migrations/20220503141649_createAttractions.cjs
@@ -9,7 +9,7 @@ exports.up = async (knex) => {
   return knex.schema.createTable("attractions", (table) => {
     table.bigIncrements("id")
     table.string("name").notNullable()
-    table.string("description")
+    table.text("description")
     table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
     table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
   })
