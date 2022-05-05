1a586bb51fd09ef219ee27035f0295a1359d6a20
camziny <cameronziny@gmail.com>
2022-05-05 15:35:15 -0400

    rootRouter fixed

--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -2,13 +2,13 @@ import express from "express";
 import userSessionsRouter from "./api/v1/userSessionsRouter.js";
 import usersRouter from "./api/v1/usersRouter.js";
 import clientRouter from "./clientRouter.js";
-import attractionsRouter from './api/v1/attractionsRouter.js'
+import attractionsRouter from "./api/v1/attractionsRouter.js";
 
 const rootRouter = new express.Router();
 
 rootRouter.use("/", clientRouter);
 rootRouter.use("/api/v1/user-sessions", userSessionsRouter);
 rootRouter.use("/api/v1/users", usersRouter);
-rootRouter.use("/api/v1/attractions", attractionsRouter)
+rootRouter.use("/api/v1/attractions", attractionsRouter);
 
 export default rootRouter;
