db2083f3cbaec5f32d3b62f64164984aa134372f
ALEX CAMERON <acameron724@gmail.com>
2022-05-11 10:20:21 -0400

    add user authentication for reviewDelete

--- client/src/components/ReviewTile.js
+++ client/src/components/ReviewTile.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-const ReviewTile = ({ id, title, rating, content, deleteReview }) => {
+const ReviewTile = ({ id, title, rating, content, deleteReview, isOwner }) => {
+  const deleteButton = isOwner ? (
+    <input
+      type="button"
+      value="Delete Review"
+      onClick={() => {
+        deleteReview(id);
+      }}
+    />
+  ) : null;
 
   return (
     <div>
       <h4>{title}</h4>
       <h5>{rating}</h5>
       <p>{content}</p>
-      <div onClick={() => { deleteReview(id) }}>
-        <input type="submit" value="Delete Review"/>
-      </div>
+      {deleteButton}
+      <hr/>
     </div>
   );
 };
